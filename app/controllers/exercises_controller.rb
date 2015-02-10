e7058d30b08f615d71616583472dc7071c0210e2
Alan <alanng95@hotmail.com>
2015-02-10 12:53:40 +1100

    added methods to the exercise controller and added the backbone models and collections

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,6 +1,32 @@
 class ExercisesController < ApplicationController
+before_action :find_exercise, only: [:destroy, :update]
   def index
     @exercises = Exercise.all
     render json: @exercises
+  end
+
+  def create
+    @exercise = Exercise.create(exercise_params)
+    render json: @exercise
+  end
+
+  def update
+    @exercise.update(exercise_params)
+    render json: @exercise
+  end
+
+  def destroy
+    @exercise.destroy
+    render json: { status: "OK"}
+  end
+
+  private
+
+  def find_exercise
+    @exercise = Exercise.find(params[:id])
+  end
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :featured, :intensity, :description, :time, :calories, :exercise_category_id)
   end
 end
